44bf1b95e9893a92db8f6bcd31b532e9fd4dc17b
govind-geethub <avin14a@gmail.com>
2025-07-11 12:53:58 +0530

    numberOfPositions cpp and exe file

---
+++ rating-1300/numberOfPositions.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int numberOfPositions(int n, int a, int b)
+{
+    int i;
+    for(i=1; i<=n; i++)
+    {
+        int count1=0;
+        for(int j=1; j<i; j++) 
+        {
+            count1++;
+        }
+        // cout << count1 << endl;
+        int count2=0;
+        for(int k=i+1; k<=n; k++) 
+        {
+            count2++;
+        }
+        // cout << count2 << endl;
+
+        if(count1>=a && count2<=b) break;
+    }
+    return i;
+}
+int main()
+{
+    int n,a,b;
+    cin >> n >> a >> b;
+    cout << numberOfPositions(n,a,b) << endl;
+    return 0;
+}
